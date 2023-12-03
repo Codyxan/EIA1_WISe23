28ef4fa51ffa5fdf8476f5a217b01fcd84e465b0
Codyxan <149688009+Codyxan@users.noreply.github.com>
2023-12-03 17:47:23 +0100

    eigentlich will ich nur L05 aber hier sind viel mehr sachen drin egal
    ich weiß nicht ob die aufgabe auch so richtig war egal

---
+++ L05/script.js
@@ -0,0 +1,19 @@
+let StoryStart = prompt(
+    "Dein Auto ist zugeschneit und du kommst nicht rechtzeitig zur Uni.......auf einer Rechten Schulter sitzt ein Teufel der sagt dir geh nach hause und scheiß auf die Uni und ein Engel auf deiner Linken Schulter jetzt musst du entscheiden schreib rechts oder links"
+)
+
+if (StoryStart == "rechts" || StoryStart == "Rechts"|| StoryStart == "r") {
+   let  = prompt(" Du drehst um und gehst nach Hause bist du PRODUKTIV und machst den Stoff daheim oder gehst du ins BETT ")
+}
+
+else if (StoryStart == "links"|| StoryStart == "Links"|| StoryStart == "l") {
+    prompt(" Du versuchst dein Auto freizuschaufeln jedoch ist es zuviel Schnee dein Auto steckt immernoch fest und du bist verschwitzt und gehst du nach Hause  ")
+}
+
+else {
+     StoryStart= prompt ('schreib rechts oder links')
+}
+
+let StoryR = prompt ( 
+    "Du legst dich wieder in dein Bett egal ob du produktiv sein wolltest und pennst den ganzen tag"
+)
